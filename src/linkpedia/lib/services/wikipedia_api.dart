c76038d738066a950f7142b01f831c7bd15c5d26
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2023-03-08 09:45:06 +0000

    refactor: extracted article summary fetching to its own method

--- src/linkpedia/lib/services/wikipedia_api.dart
+++ src/linkpedia/lib/services/wikipedia_api.dart
@@ -19,10 +19,10 @@ class WikipediaService {
     // ignore: prefer_interpolation_to_compose_strings
     String url = _wikipediaBaseUrl
       + 'action=opensearch&'
-      + 'format=json&' // get output as JSON
-      + "limit=$numberOfArticles&" // Limit to $numberOfArticles articles
-      + 'origin=*&' // something to do with CORS
-      + "search=$searchQuery"; // search parameter
+      + 'format=json&'              // get output as JSON
+      + "limit=$numberOfArticles&"  // Limit to $numberOfArticles articles
+      + 'origin=*&'                 // something to do with CORS
+      + "search=$searchQuery";      // search parameter
 
     final searchResponse = await http.get(Uri.parse(url));
 
@@ -38,34 +38,38 @@ class WikipediaService {
     for (int i = 0; i < numberOfArticles; i++) {
       String articleTitle = searchData[1][i];
       String articleUrl = searchData[3][i];
-
-      /* Wikipedia API Query Url */
-      // ignore: prefer_interpolation_to_compose_strings
-      url = _wikipediaBaseUrl
-        + 'action=query&'
-        + 'prop=extracts&' // extract Summary
-        + 'exchars=$numberOfCharacters&' // cap extraction at $numberOfCharacters chars
-        + 'exlimit=1&' // limit to one article
-        + 'explaintext=True&' // fetch data as plain text instead of HTML
-        + 'format=json&' // format to JSON
-        + 'formatversion=2&' // removes need to get pageId before hand
-        + 'origin=*&' // something to do with CORS
-        + "titles=$articleTitle";
-
-      final extractResponse = await http.get(Uri.parse(url));
-
-      /* throw exception if there was an error fetching data */
-      if (extractResponse.statusCode != 200) {
-        throw Exception("Failed to load from Wikipedia API");
-      }
-
-      final extractData = jsonDecode(extractResponse.body);
-      String articleSummary = extractData['query']['pages'][0]['extract'];
+      String articleSummary = await fetchArticleSummary(articleTitle);
 
       articles.add(WikiArticle(title: articleTitle, summary: articleSummary, url: articleUrl));
     }
 
     return articles;
+  }
+
+  Future<String> fetchArticleSummary(String articleTitle) async {
+    /* Wikipedia API Query Url */
+    // ignore: prefer_interpolation_to_compose_strings
+    String url = _wikipediaBaseUrl
+      + 'action=query&'
+      + 'prop=extracts&'                // extract Summary
+      + 'exchars=$numberOfCharacters&'  // cap extraction at $numberOfCharacters chars
+      + 'exlimit=1&'                    // limit to one article
+      + 'explaintext=True&'             // fetch data as plain text instead of HTML
+      + 'format=json&'                  // format to JSON
+      + 'formatversion=2&'              // removes need to get pageId before hand
+      + 'origin=*&'                     // something to do with CORS
+      + "titles=$articleTitle";
+
+    final extractResponse = await http.get(Uri.parse(url));
+
+    /* throw exception if there was an error fetching data */
+    if (extractResponse.statusCode != 200) {
+      throw Exception("Failed to load from Wikipedia API");
+    }
+
+    final extractData = jsonDecode(extractResponse.body);
+    
+    return extractData['query']['pages'][0]['extract'];
   }
 }
 
@@ -73,7 +77,7 @@ class WikipediaService {
  * Refer to these urls for JSON response structure
  * 
  * OpenSearch: https://www.mediawiki.org/wiki/API:Opensearch#Response
- * * Note: the second list of the json is supposed to return the article summaries but it returns an empty string
+ * * Note: the second list of the json should return the article summaries but it returns an empty string
  * 
  * Query: https://www.mediawiki.org/wiki/API:Query#Response
  */
