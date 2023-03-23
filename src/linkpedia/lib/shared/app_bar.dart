4f5f3035b708e99886cf961222b9e449cec8785b
sirkotal <lordkotal2002@gmail.com>
2023-03-23 15:58:06 +0000

    Prototype of the Bar class

--- src/linkpedia/lib/shared/app_bar.dart
+++ src/linkpedia/lib/shared/app_bar.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
 class Bar extends StatelessWidget {
+  const Bar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Linkpedia',
-      theme: ThemeData(),
-    )
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Linkpedia'),
+        ),
+        body: const Center(
+          child: Text('Among Us'),
+        ),
+      ),
+    );
   }
 }
