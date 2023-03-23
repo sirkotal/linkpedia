ca88a29c7976032e3301c1a5d178eeb68c704c64
sirkotal <lordkotal2002@gmail.com>
2023-03-23 15:55:33 +0000

    Introduced the Bar class

---
+++ src/linkpedia/lib/shared/app_bar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Bar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Linkpedia',
+      theme: ThemeData(),
+    )
+  }
+}
