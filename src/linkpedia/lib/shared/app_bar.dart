ecbdf85eec55e9598280b7d7af0ef3315deff5bc
sirkotal <lordkotal2002@gmail.com>
2023-03-29 11:17:35 +0100

    Refactored the App Bar's code

--- src/linkpedia/lib/shared/app_bar.dart
+++ src/linkpedia/lib/shared/app_bar.dart
@@ -1,46 +1,43 @@
 import 'package:flutter/material.dart';
 
 class TopBar extends StatelessWidget implements PreferredSizeWidget {
-  const TopBar({super.key});
+  final String title;
+
+  const TopBar({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      leading: IconButton(
-        icon: const Icon(Icons.menu),
-        onPressed: () => {
-          Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) => const HomePage()))
-        },
-      ),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.settings),
-          onPressed: () => {
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => const SettingsPage()))
-          },
-        ),
-      ],
-      iconTheme: const IconThemeData(
-        color: Colors.deepPurple,
-        size: 36.0,
-      ),
-      backgroundColor: Colors.white,
-      elevation: 0.0,
-      bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(2.0),
-        child: Container(
-          decoration: const BoxDecoration(
-            border: Border(
-              top: BorderSide(
-                color: Colors.deepPurple,
-                width: 2.0,
-              ),
+    return Column(
+      children: <Widget>[
+        AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const HomePage()))
+            },
+          ),
+          title: Text(title),
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+            size: 36.0,
+          ),
+          backgroundColor: Colors.deepPurple,
+          elevation: 0.0,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.elliptical(50, 25),
             ),
           ),
         ),
-      ),
+        ClipPath(
+          clipper: BottomLeftArcClipper(),
+          child: Container(
+            color: Colors.deepPurple,
+            height: kToolbarHeight,
+          ),
+        )
+      ],
     );
   }
 
