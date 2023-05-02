4d926b956bf6227cc0cca4e8eabc5953c935c726
sirkotal <lordkotal2002@gmail.com>
2023-05-02 21:22:16 +0100

    Reworked the App Bar (again)

--- src/linkpedia/lib/shared/app_bar.dart
+++ src/linkpedia/lib/shared/app_bar.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../screens/home_page/home_page.dart';
+
 class TopBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
 
-  const TopBar({super.key, required this.title});
+  const TopBar({super.key, required this.title, required IconButton leading});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () => {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const HomePage()))
-            },
-          ),
           title: Text(title),
           iconTheme: const IconThemeData(
             color: Colors.white,
@@ -26,45 +21,15 @@ class TopBar extends StatelessWidget implements PreferredSizeWidget {
           elevation: 0.0,
           shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
-              bottomRight: Radius.elliptical(50, 25),
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25),
             ),
           ),
         ),
-        ClipPath(
-          clipper: BottomLeftArcClipper(),
-          child: Container(
-            color: Colors.deepPurple,
-            height: kToolbarHeight,
-          ),
-        )
       ],
     );
   }
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-}
-
-class BottomLeftArcClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    const rad = Radius.elliptical(75, 50);
-
-    path.lineTo(0, size.height);
-    path.lineTo(0, rad.y);
-    path.arcToPoint(
-      Offset(rad.x, 0),
-      radius: rad,
-      clockwise: true,
-    );
-    path.close();
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
-  }
 }
