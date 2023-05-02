a5e3e71aa1c5ad7ab093b09283966558eb26ea54
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2023-05-02 18:40:10 +0100

    added comment reaction and update comment method to the service

--- src/linkpedia/lib/models/comment.dart
+++ src/linkpedia/lib/models/comment.dart
@@ -4,15 +4,17 @@ class Comment {
   final String commentId; /* use uuid: `Uuid().v4()` */
   final String userId;
   final String articleUrl;
-  final String commentBody;
+  String commentBody;
   final DateTime timestamp; /* use microseconds since epoch: DateTime.now().microsecondsSinceEpoch.toString() */
+  int votes;
 
   Comment({
     required this.commentId,
     required this.userId,
     required this.articleUrl,
     required this.commentBody,
-    required this.timestamp
+    required this.timestamp,
+    this.votes = 0
   });
 
   String timestampToString() {
