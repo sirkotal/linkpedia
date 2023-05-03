053fc7dc576da9925ac28b4a9b467711339ebb55
sirkotal <lordkotal2002@gmail.com>
2023-05-03 10:44:33 +0100

    Implemented some changes into the reworked App Bar

--- src/linkpedia/lib/shared/app_bar.dart
+++ src/linkpedia/lib/shared/app_bar.dart
@@ -1,32 +1,27 @@
 import 'package:flutter/material.dart';
 
-import '../screens/home_page/home_page.dart';
-
 class TopBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
+  final IconButton? leading;
 
-  const TopBar({super.key, required this.title, required IconButton leading});
+  const TopBar({super.key, required this.title, this.leading});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        AppBar(
-          title: Text(title),
-          iconTheme: const IconThemeData(
-            color: Colors.white,
-            size: 36.0,
-          ),
-          backgroundColor: Colors.deepPurple,
-          elevation: 0.0,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(25),
-              bottomLeft: Radius.circular(25),
-            ),
-          ),
+    return AppBar(
+      title: Text(title),
+      leading: this.leading,
+      iconTheme: const IconThemeData(
+        color: Colors.white,
+        size: 36.0,
+      ),
+      elevation: 0.0,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
         ),
-      ],
+      ),
     );
   }
 
