02e33502e3ef7d6ecff698b5c10cc5ada0e01267
sirkotal <lordkotal2002@gmail.com>
2023-05-24 20:20:04 +0100

    Small changes to the Edit Profile's page

--- src/linkpedia/lib/screens/profile/edit_profile.dart
+++ src/linkpedia/lib/screens/profile/edit_profile.dart
@@ -45,14 +45,14 @@ class _EditProfilePageState extends State<EditProfilePage> {
     }).then((_) {
       // Update successful
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Profile updated successfully')),
+        const SnackBar(content: Text('Profile updated successfully!')),
       );
     }).catchError((error) {
       // Handle error
       // ignore: avoid_print
       print('Failed to update profile: $error');
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Failed to update profile')),
+        const SnackBar(content: Text('Failed to update your profile')),
       );
     });
   }
@@ -65,23 +65,46 @@ class _EditProfilePageState extends State<EditProfilePage> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Text(
-                'Current Username: ${widget.userData.username}',
-                style: TextStyle(fontSize: 24.0),
+              Container(
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.deepPurple, width: 2.0),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Text(
+                    'Current Username: ${widget.userData.username}',
+                    style: const TextStyle(
+                        fontSize: 20.0, fontFamily: 'Times New Roman'),
+                  ),
+                ),
               ),
               TextFormField(
                 controller: _usernameController,
                 decoration: InputDecoration(labelText: 'Username'),
               ),
-              Text(
-                'Current Name: ${widget.userData.name}',
-                style: TextStyle(fontSize: 24.0),
+              const SizedBox(height: 32),
+              Container(
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.deepPurple, width: 2.0),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Text(
+                    'Current Username: ${widget.userData.name}',
+                    style: const TextStyle(
+                        fontSize: 20.0, fontFamily: 'Times New Roman'),
+                  ),
+                ),
               ),
               TextFormField(
                 controller: _nameController,
                 decoration: InputDecoration(labelText: 'Name'),
               ),
-              SizedBox(height: 16.0),
+              const SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: _updateProfile,
                 style: ButtonStyle(
